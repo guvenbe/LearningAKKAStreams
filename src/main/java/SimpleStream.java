5fbfc00601a95ac48b27eed883cca15e8ab3145c
Bora Guven <guvenbe@gmail.com>
2022-05-15 16:46:13 -0400

    init

--- src/main/java/SimpleStream.java
+++ src/main/java/SimpleStream.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class SimpleStream {
+import akka.Done;
+import akka.NotUsed;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
+
+import java.util.concurrent.CompletionStage;
+
+public class SimpleStream {
+    public static void main(String[] args) {
+        Source<Integer, NotUsed> range = Source.range(1, 10);
+        Sink<String, CompletionStage<Done>> sink = Sink.foreach(value ->{
+            System.out.println(value);
+        });
+    }
 }
