59f6e742297e369587759b2f8b11d82e3ad435b7
Bora Guven <guvenbe@gmail.com>
2022-05-28 14:19:28 -0400

    Flow generic types

--- src/main/java/ExploringFlows.java
+++ src/main/java/ExploringFlows.java
@@ -6,6 +6,9 @@ import akka.stream.javadsl.Flow;
 import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletionStage;
 
@@ -21,9 +24,22 @@ public class ExploringFlows {
                     List<Integer> result = List.of(value, value + 1, value +2);
                     return  result;
                 });
-        Flow<Integer, List<Integer>, NotUsed> groupFlow = Flow.of(Integer.class)
-                .grouped(3);
-        Sink<List<Integer>, CompletionStage<Done>> printsink =  Sink.foreach(System.out::println);
+//        Flow<Integer, List<Integer>, NotUsed> groupFlow = Flow.of(Integer.class)
+//                .grouped(3);
+        Flow<Integer, Integer, NotUsed> groupFlow = Flow.of(Integer.class)
+                .grouped(3)
+                .map(value -> {
+                    List<Integer> newList = new ArrayList<>(value); //by passing value to constructor we create copy of
+                                                                    // our list. Ths is the list we can mutate
+                    Collections.sort(newList, Collections.reverseOrder());
+                    return newList;
+                })
+                .mapConcat(value->value); //This knows how to handle List -> List
+        //Flow<IntegerList, Integer, NotUsed> unGroupedFlow =Flow.of(IntegerList.class).mapConcat(value -> null);
+        //Flow<List, Integer, NotUsed> unGroupedFlow =Flow.of(List.class).mapConcat(value -> value);
+
+
+        Sink<Integer, CompletionStage<Done>> printsink =  Sink.foreach(System.out::println);
 
         number.via(filterFlow).via(mapConcatFlow).via(groupFlow).to(printsink).run(actorSystem);
     }
